e7b029564491c6f360cef59119fcab39aa24e0e5
Johnson-Spencer <spencerjohnsonemail@gmail.com>
2025-09-19 21:20:39 -0600

    added cout come for grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your Grade percentage?(Number vales only please)");
+        string Number = Console.ReadLine();
+        int percent = int.Parse(Number);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (grade == "A" || grade == "B" || grade == "C")
+        {
+            Console.WriteLine("Congratulations, you did it, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Please see a TA or the Teacher if you need help!");
+        }
     }
+
+    
 }

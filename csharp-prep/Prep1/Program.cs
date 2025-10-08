d4acbfd639b22296cfd43dc9b7eddcebed34cf7e
Johnson-Spencer <spencerjohnsonemail@gmail.com>
2025-10-08 16:10:33 -0600

    test push

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -12,5 +12,24 @@ class Program
 
         Console.Write($"Your name is {lastname}, {firstname} {lastname}.");
 
+
+        List<int> mylist = new List<int>();
+        mylist.Add(10);
+        mylist.Add(20);
+        mylist.Add(30);
+
+        int count = mylist.count();
+
+        counsle.writeline($"The counnt is: {count}");
+
+        int total = 0;
+        for (int i = 0; i < mylist.Count; i++)
+        {
+            total = total + mylist[i];
+        }
+
+        Console.WriteLine($"The total is: {total}");
+
+
     }
 }
